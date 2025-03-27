46f487f46592e88b0ce8b8656d7e2ac375383722
Richie james <rjxl17@gmail.com>
2025-03-27 13:46:44 -0400

    Added Server Connection

--- Richie/RichieWork.py
+++ Richie/RichieWork.py
@@ -1,1 +1,18 @@
+import pyodbc
 
+class DatabaseManagement:
+    def connect_to_database(self):
+        '''
+        Connect to our SQL Server instance
+        @return the connection object or none on failure
+        '''
+        try:
+            conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
+                      'Database=IS4010;'
+                      'uid=IS4010Login;'
+                      'pwd=P@ssword2;')
+        except:
+            return None
+
+            return conn
